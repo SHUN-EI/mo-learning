188ea1b745435c2f0b0781fcc120fb09f0b40c51
moweiquan <295597613@qq.com>
2023-11-22 00:01:45 +0800

    springboot 整合 knife4j 项目

---
+++ knife4j-demo/src/main/java/com/mo/SwaggerDemoApplication.java
@@ -0,0 +1,15 @@
+package com.mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mo on 2023/11/21
+ */
+@SpringBootApplication
+public class SwaggerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerDemoApplication.class, args);
+    }
+}
