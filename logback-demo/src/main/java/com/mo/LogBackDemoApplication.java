2e0fdf8d0ef280c75b053f1ca845b3ee664caaee
moweiquan <295597613@qq.com>
2023-11-24 16:09:03 +0800

    springboot 整合 logback 项目

---
+++ logback-demo/src/main/java/com/mo/LogBackDemoApplication.java
@@ -0,0 +1,15 @@
+package com.mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mo on 2023/11/24
+ */
+@SpringBootApplication
+public class LogBackDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogBackDemoApplication.class, args);
+    }
+}
