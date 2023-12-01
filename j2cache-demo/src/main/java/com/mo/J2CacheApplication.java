0ad5bd9209b1a941d5ff2a5dd1bbc7b618b2537a
moweiquan <295597613@qq.com>
2023-12-01 20:29:14 +0800

    springboot 整合j2cache项目

---
+++ j2cache-demo/src/main/java/com/mo/J2CacheApplication.java
@@ -0,0 +1,15 @@
+package com.mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mo on 2023/12/1
+ */
+@SpringBootApplication
+public class J2CacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J2CacheApplication.class, args);
+    }
+}
